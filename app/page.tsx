22431309740f44e30758b10a5a81ccb594a4fede
Allen Davis-Swing <allen.davisswing@gmail.com>
2024-05-02 01:28:51 -0400

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import { ChevronRightIcon } from '@radix-ui/react-icons'
 import { Centered } from './_components/Center'
 import { Playfair_Display } from 'next/font/google'
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 const playfairDisplay = Playfair_Display({ subsets: ['latin'] })
 
@@ -27,7 +30,20 @@ export default async function Home() {
     const numRaindrops = 100
     return (
         <main className="h-screen w-screen">
-            <AllenHero />
+            <Centered className="mx-5 md:w-2/3 md:mx-auto break-words p-1">
+                <div className="flex flex-row gap-2 items-center justify-start w-fit mr-auto mb-2">
+                    <div className='text-xl'>{`I'm Allen`}</div>
+                    <Badge className="bg-purple-500">back end</Badge>
+                    <Badge className="bg-teal-500">front end</Badge>
+                    <Badge className="bg-sky-500">dev ops</Badge>
+                </div>
+                <div className="text-5xl leading-tight md:leading-snug md:mb-5 text-wrap">
+                    Crafting beautiful software to solve <Badge className="text-5xl">real-world</Badge> problems
+                </div>
+                <Link className='ml-auto' href="/work">
+                    <Button className="bg-transparent active:bg-slate-100 hover:bg-slate-100 text-slate-700 shadow-none outline outline-slate-700">more</Button>
+                </Link>
+            </Centered>
         </main>
     )
 }
